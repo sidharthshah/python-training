fae0c212609254018c92e39307480f137a7cee46
Sidharth Shah <sidharth@fafadiatech.com>
2014-12-11 12:20:05 +0530

    fixed code

--- session-5/maze_solver.py
+++ session-5/maze_solver.py
@@ -1,5 +1,7 @@
+import copy
 import types
 import random
+from compiler.ast import flatten
 
 ROWS = 3
 COLS = 3
@@ -48,7 +50,7 @@ def generate_maze():
 	if is_valid_maze(maze):
 		return maze
 	else:
-		generate_maze()
+		return None
 
 def get_cords(maze, cell_value):
 	for i in range(ROWS):
@@ -57,47 +59,90 @@ def get_cords(maze, cell_value):
 				return i,j
 
 def gen_child_state(maze):
-	print "Calling gen child state"
 	child_states = []
 	current_x, current_y = get_cords(maze, "1")
 
 	#Check if valid up action is possible
-	new_x = current_x
-	new_y = current_y - 1
-	print current_x
-	print current_y
-	print new_x
-	print new_y
-	if new_y >=0 and maze[new_x][new_y] == "0":
-		print "Generate new UP state"
-		current_child_state = list(maze)
+	new_x = current_x - 1
+	new_y = current_y
+	if new_x >=0 and maze[new_x][new_y] != "-1":
+		current_child_state = copy.deepcopy(maze)
 		current_child_state[new_x][new_y] = "1"
 		current_child_state[current_x][current_y] = "0"
 		child_states.append(current_child_state)
-		print current_child_state
 
 	#Check if valid right action is possible
-	new_x = current_x + 1
-	new_y = current_y
-	if new_x <=ROWS-1 and maze[new_x][new_y] == "0":
-		print "Generate new RIGHT state"
-		current_child_state = list(maze)
+	new_x = current_x
+	new_y = current_y + 1
+	if new_y <=COLS-1 and maze[new_x][new_y] != "-1":
+		current_child_state = copy.deepcopy(maze)
 		current_child_state[new_x][new_y] = "1"
 		current_child_state[current_x][current_y] = "0"
 		child_states.append(current_child_state)
-		print current_child_state
 	return child_states
+
+#Print Maze
 def print_maze(maze):
 	if isinstance(maze, types.NoneType):
 		return
 	for row in maze:
 		print "\t".join(row)
 
+def print_solution_maze(solution):
+	solution = solution.split(",")
+	for i in range(ROWS):
+		print "\t".join(solution[ROWS*i: (ROWS*i) + (COLS)])
+	print "\n"
+#Main
 if __name__ == "__main__":
+	#Generate a valid maze
 	maze = generate_maze()
+	while (isinstance(maze, types.NoneType)):
+		maze = generate_maze()
+
+	frontier = []
+	solution = []
+	visited = {}
+	parent_state = {}
+	found = False
+	end_x, end_y = get_cords(maze, "2")
 	print_maze(maze)
-	child_states = gen_child_state(maze)
-	print "\n\nChild States:\n\n"
-	for child_state in child_states:
-		print_maze(child_state)
-		print "***\n\n"
+	frontier = gen_child_state(maze)
+
+	parent_state[",".join(flatten(maze))] = None
+	for initial_state in frontier:
+		parent_state[",".join(flatten(initial_state))] = ",".join(flatten(maze))
+
+	while(len(frontier) != 0):
+		current_state = frontier.pop(0)
+		current_x, current_y = get_cords(current_state, "1")
+		
+		#Check if current state needs to be terminated
+		if (current_x == end_x and current_y == end_y):
+			"Print found solution"
+			found = True
+			solution.append(",".join(flatten(current_state)))
+			break
+
+		#Skip if current state has already been visited
+		if visited.has_key(",".join(flatten(current_state))):
+			continue
+
+		#Expand frontier & mark state as visited
+		child_states = gen_child_state(current_state)
+		if len(child_states) > 0 and not isinstance(child_states, types.NoneType):
+			for new_state in child_states:
+				frontier.append(new_state)
+				parent_state[",".join(flatten(new_state))] = ",".join(flatten(current_state))
+		visited[",".join(flatten(current_state))] = ""
+		
+	if not found:
+		print "\n\nNo solution exists"
+	else:
+		print "\n\nSolution Found"
+		while(not isinstance(parent_state[solution[-1]], types.NoneType)):
+			solution.append(parent_state[solution[-1]])
+
+		solution.reverse()
+		for result in solution:
+			print_solution_maze(result)
