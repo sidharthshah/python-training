34f756c5e1f3278728143fb33bbaefac8d82722f
Sidharth Shah <sidharth@fafadiatech.com>
2014-12-18 11:00:40 +0530

    written logic to write and read index

--- session-10/inverted_index.py
+++ session-10/inverted_index.py
@@ -29,10 +29,27 @@ def read_stop_words(file_name):
 		line = line.strip()
 		stop_words.append(line)
 
+def write_index(file_name):
+	file_to_write = open(file_name, "w")
+	for k,v in index.items():
+		rec = "%s\t%s\n" % (k, "\t".join(v))
+		file_to_write.write(rec)
+	file_to_write.close()
+
+def read_index(file_name):
+	file_to_read = open(file_name)
+	for line in file_to_read.readlines():
+		line = line.strip()
+		line = line.split()
+		k,v = line[0], line[1:]
+		index[k] = v
 
 def print_options():
-	print "1: Search"
-	print "2: Quit"
+	print "1: Read Index"
+	print "2: Write Index"
+	print "3: Index for Input Directory"
+	print "4: Search"
+	print "5: Quit"
 
 if __name__ == "__main__":
 
@@ -43,17 +60,26 @@ if __name__ == "__main__":
 
 	dir_name = sys.argv[1]
 	read_stop_words("stoplist.txt")
-	files_to_process = os.listdir(dir_name)
-	index_files(dir_name, files_to_process)
-
-	print "Completed Indexing"
 
 	print_options()
 	choice = raw_input("Please enter your choice:")
 
 
-	while choice != "2":
+	while choice != "5":
 		if choice == "1":
+			file_to_read = raw_input("Please enter name of index to read:")
+			read_index(file_to_read)
+
+		if choice == "2":
+			file_to_save_name = raw_input("Please enter name of index to write:")
+			write_index(file_to_save_name)
+
+		if choice == "3":
+			files_to_process = os.listdir(dir_name)
+			index_files(dir_name, files_to_process)
+			print "Completed Indexing"
+
+		if choice == "4":
 			query = raw_input("Please enter the word you want to search:")
 
 			if not index.has_key(query):
@@ -61,5 +87,5 @@ if __name__ == "__main__":
 			else:
 				print index[query]
 
-			print_options()
-			choice = raw_input("Please enter your choice:")			
+		print_options()
+		choice = raw_input("Please enter your choice:")			
