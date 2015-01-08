314113e1e79c5e09e51133e541c9f90b702248f4
Sidharth Shah <sidharth@fafadiatech.com>
2015-01-08 10:11:37 +0530

    updated final code

--- project-1/trade.py
+++ project-1/trade.py
@@ -5,10 +5,19 @@ from datetime import datetime, timedelta
 
 URL = "http://www.bseindia.com/download/BhavCopy/Equity/EQ%s_CSV.ZIP"
 
+data = {}
+
+def update_data(row, ts):
+	script,open, high, low, close = row[1], row[4], row[5], row[6], row[7]
+	script = script.strip()
+	if not data.has_key(script):
+		data[script] = {}
+	data[script][ts] = [open, high, low, close]
+
 if __name__ == "__main__":
 	today = datetime.now()
-	for i in range(32):
-		current_date = today - timedelta(days=i)
+	for i in range(15):
+		current_date = today - timedelta(days=i+1)
 		TS = "%0.2d%0.2d%s" % (int(str(current_date.day)), int(str(current_date.month)),str(current_date.year)[2:])
 		DOWNLOAD_URL = URL % TS
 		CMD = "wget %s" % DOWNLOAD_URL
@@ -24,9 +33,20 @@ if __name__ == "__main__":
 			with file_to_read as csvFile:
 				csvReader = csv.reader(csvFile, quotechar="|")
 				for row in csvReader:
-					print ','.join(row)
-
+					update_data(row, TS)
 		else:
 			print read_file+" do not exists"
 			continue
+
+	script = raw_input("Enter Script you want to get data of:")
+	while script != "-1":
+		if not data.has_key(script):
+			print "Script Not found"
+		else:
+			all_ts = data[script].keys()
+			all_ts.sort()
+			for ts in all_ts:
+				print ts,"\t","\t".join(data[script][ts])
+		print "\n"
+		script = raw_input("Enter Script you want to get data of:")
 
